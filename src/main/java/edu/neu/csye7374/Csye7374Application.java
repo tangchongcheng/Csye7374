247b759b97ad134cf02c4c5ebdb266fea7bf57f1
tangchongcheng <tang.cho@husky.neu.edu>
2022-04-29 14:20:13 -0700

    init

---
+++ src/main/java/edu/neu/csye7374/Csye7374Application.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csye7374Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csye7374Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neu/csye7374/Csye7374ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csye7374ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
