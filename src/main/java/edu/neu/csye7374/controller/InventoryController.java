912745deca0e1b7220f06761136d8e8be5824445
tangchongcheng <tang.cho@husky.neu.edu>
2022-04-30 14:48:25 -0700

    inventory 2

--- src/main/java/edu/neu/csye7374/controller/InventoryController.java
+++ src/main/java/edu/neu/csye7374/controller/InventoryController.java
@@ -1,8 +1,9 @@
 package edu.neu.csye7374.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class InventoryController {
+
+
 }
