a85cde5ba5a2a29f1cc7f479e73b321d2458f736
--Shawson Tang <tang.sha@northeastern.edu>
2022-04-30 17:00:25 -0700

    change order ids

--- src/main/java/edu/neu/csye7374/facade/CartFacade.java
+++ src/main/java/edu/neu/csye7374/facade/CartFacade.java
@@ -1,53 +1,52 @@
 package edu.neu.csye7374.facade;
 
-import edu.neu.csye7374.entity.Order;
+import edu.neu.csye7374.entity.PSOrder;
 import edu.neu.csye7374.entity.item.*;
 import edu.neu.csye7374.facade.cart.*;
 import edu.neu.csye7374.facade.cart.game.EldenRingAdd;
 import edu.neu.csye7374.facade.cart.game.GameAPI;
 import edu.neu.csye7374.facade.cart.game.Persona5Add;
 import edu.neu.csye7374.facade.order.OrderBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class CartFacade {
     CartAPI cart;
-    Order order;
+    PSOrder PSOrder;
     OrderBuilder orderBuilder;
 
     public CartFacade(){
         cart = new Cart();
         orderBuilder = new OrderBuilder();
-        order = orderBuilder.init().setItems(cart).build();
+        PSOrder = orderBuilder.init().setItems(cart).build();
     }
 
     public void addController(Controller controller){
         cart = new ControllerAdd(cart, controller);
-        order = orderBuilder.setItems(cart).build();
+        PSOrder = orderBuilder.setItems(cart).build();
     }
 
     public void addMonitor(Monitor monitor){
         cart = new MonitorAdd(cart, monitor);
-        order = orderBuilder.setItems(cart).build();
+        PSOrder = orderBuilder.setItems(cart).build();
     }
 
     public void addPlayStation(PlayStation playStation){
         cart = new PlayStationAdd(cart, playStation);
-        order = orderBuilder.setItems(cart).build();
+        PSOrder = orderBuilder.setItems(cart).build();
     }
 
     public void addEldenRing(EldenRing eldenRing){
         GameAPI eldenRingAdd = new EldenRingAdd(eldenRing);
         cart = new GameAdaptor(cart, eldenRingAdd);
-        order = orderBuilder.setItems(cart).build();
+        PSOrder = orderBuilder.setItems(cart).build();
     }
 
     public void addPersona5(Persona5 persona5){
         GameAPI persona5Add = new Persona5Add(persona5);
         cart = new GameAdaptor(cart, persona5Add);
-        order = orderBuilder.setItems(cart).build();
+        PSOrder = orderBuilder.setItems(cart).build();
     }
 
-    public Order getOrder(){
-        return order;
+    public PSOrder getPSOrder(){
+        return PSOrder;
     }
 }
