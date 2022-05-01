28e5c48622e1602d64cb841e5d31bac4226a02f4
--Shawson Tang <tang.sha@northeastern.edu>
2022-04-30 18:35:40 -0700

    deliver order

--- src/main/java/edu/neu/csye7374/controller/PersonnelController.java
+++ src/main/java/edu/neu/csye7374/controller/PersonnelController.java
@@ -1,25 +1,45 @@
 package edu.neu.csye7374.controller;
 
 import edu.neu.csye7374.entity.Employee;
-import edu.neu.csye7374.service.InventoryService;
+import edu.neu.csye7374.entity.PSOrder;
+import edu.neu.csye7374.service.PersonnelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Arrays;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class PersonnelController {
     @Autowired
-    private InventoryService inventoryService;
+    private PersonnelService personnelService;
 
-//    @GetMapping("/user")
-//    public String loginForm(Model model) {
-//        model.addAttribute("user", new Employee());
-//        List<String> roles = Arrays.asList("Employee", "Admin");
-//        model.addAttribute("roles", roles);
-//        return "employee";
-//    }
+    @GetMapping("/user")
+    public String getUser(Model model, HttpSession httpSession) {
+        Employee user = (Employee) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+            if (user.getRole().equalsIgnoreCase("Employee")) {
+                List<PSOrder> psOrders = personnelService.getOrderByIds(user.getOrderIds());
+                model.addAttribute("psOrders", psOrders);
+                return "employee";
+            }
+            return "admin";
+        }
+        return "403";
+    }
+
+    @GetMapping("/delivered/{orderId}")
+    public String updateOrderStatus(@PathVariable(name = "orderId") int orderId) {
+        personnelService.updateOrderById(orderId);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/signout")
+    public String signOut() {
+        return "redirect:/";
+    }
 }
